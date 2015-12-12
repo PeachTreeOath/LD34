e1ee79f4fd2e45a3c17438b673c9073a7478ec12
Tim <tmizas@gmail.com>
2015-12-12 13:52:08 -0500

    Added production sliders.

--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -6,4 +6,14 @@ public class Globals : MonoBehaviour {
 	public static float musicVolume = 100;
 	public static float sfxVolume = 25;
 	public static GameObject bgMusic;
+	public static GameState gameState = null;
+
+	void Start()
+	{
+		if(gameState == null)
+		{
+			gameState = ScriptableObject.CreateInstance<GameState>();
+			gameState.productionRates = new System.Collections.Generic.List<float>();
+		}
+	}
 }
---
+++ Assets/Scripts/SliderController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SliderController : MonoBehaviour {
+
+	public float MaxX;
+	public float MinX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
