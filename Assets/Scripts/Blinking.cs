565910e58841448b64f01bb841a66638529fc432
mjeffery <michael.jeffery@gmail.com>
2015-12-12 20:59:10 -0500

    add friction, slow death to caravan

---
+++ Assets/Scripts/Blinking.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinking : MonoBehaviour {
+
+    private bool blinking = false;
+
+    public float blinkDelay = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Blink());
+	}
+
+    void Update()
+    {
+        if (!blinking) StartCoroutine(Blink());
+    }
+    	
+	private IEnumerator Blink()
+    {
+        SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+        blinking = true;
+
+        while (enabled && blinking)
+        {
+            renderer.enabled = false;
+            yield return new WaitForSeconds(blinkDelay);
+            renderer.enabled = true;
+            yield return new WaitForSeconds(blinkDelay);
+        }
+
+        renderer.enabled = true;
+        blinking = false;
+    }
+}
