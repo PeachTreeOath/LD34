a8682095350303d59759e8af8fe783cda17f9964
Tim <tmizas@gmail.com>
2015-12-13 15:20:31 -0500

    UI updates

--- Assets/Scripts/TradeGood.cs
+++ Assets/Scripts/TradeGood.cs
@@ -6,11 +6,19 @@ public class TradeGood : MonoBehaviour {
 	public Globals.GoodTypeEnum goodType;
     public GameObject spawner { get; set; }
 
+	Vector3 offset;
+
 	void Start()
 	{
+		offset = gameObject.transform.position - Globals.playerCity.transform.position;
 		string iconName = goodType.ToString().ToLower();
 		iconName = iconName.Substring(0, 1).ToUpper() + iconName.Substring(1);
 		gameObject.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Textures/Trade Goods/"+iconName);
+	}
+
+	void Update()
+	{
+		gameObject.transform.position = Globals.playerCity.transform.position + offset;
 	}
 
 	public void OnDestroy()
