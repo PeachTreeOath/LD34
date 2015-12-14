ab5819313b5c9f4ba1b70963081ec45a61ab2aaf
Tim <tmizas@gmail.com>
2015-12-13 21:55:51 -0500

    adding sfx

---
+++ Assets/Scripts/TimedObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedObject : MonoBehaviour {
+
+	public float lifeTime;
+	float lifeTimer;
+
+	// Use this for initialization
+	void Start () {
+		lifeTimer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - lifeTimer > lifeTime)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
