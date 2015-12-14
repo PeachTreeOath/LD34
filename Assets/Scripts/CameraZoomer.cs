9034b9ce565d90900814b1416bdb2f60518a3b8d
mjeffery <michael.jeffery@gmail.com>
2015-12-13 20:14:48 -0500

    added some camera zooming to first level

---
+++ Assets/Scripts/CameraZoomer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoomer : MonoBehaviour {
+
+    private float minSize;
+
+    public float maxSize;
+    public float zoomSpeed = 1f;
+
+	// Use this for initialization
+	void Start () {
+        minSize = Camera.main.orthographicSize;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currSize = Camera.main.orthographicSize;
+        float delta = Input.GetAxis("Mouse ScrollWheel");
+
+        Debug.Log("delta: " + delta);
+
+        float newSize = Mathf.Clamp(currSize + delta * -zoomSpeed, minSize, maxSize);
+
+        Camera.main.orthographicSize = newSize;
+	}
+}
